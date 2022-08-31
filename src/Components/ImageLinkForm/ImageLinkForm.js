0121a15cc7b121d46c77f39f839f3bd1a2526c88
MohammadNomankhan <khannoman1702@gmail.com>
2022-08-31 16:52:14 +0530

    media query for image link form and other changes again

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = ({onInput , onSubmit}) => {
 					{`This Magic Brain will detect faces in your pictures. Give it a try`}
 			</p>
 			<div className='center'>
-				<div className='form pa4 br3 shadow-5 center'>
+				<div className='form pa4 br3 shadow-5 center align-it'>
 					<input className='f4 pa2 w-70 center' type='text' onChange = {onInput}/>
 					<button className='w-30 grow f4 ph3 pv2 dib white bg-light-purple' onClick = {onSubmit}>
 						Detect
